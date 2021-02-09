b1ecb1725a3a77768b1d3bffb2fa9325967cc7a7
Alexey Khabirov <Khabirov.A@citilink.ru>
2021-02-09 22:06:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let res = '';
+    const str = Math.abs(n).toString();
+
+    for (const char of str) {
+        res = char + res;
+    }
+
+    return Number(res);
 }
